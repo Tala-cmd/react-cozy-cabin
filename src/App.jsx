6ae5a95bed139d1c63841273167fb36b8b8bb072
Tala <70854429+Tala-cmd@users.noreply.github.com>
2024-05-14 23:02:03 +0400

    Building the app layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,25 @@ import Login from '../src/pages/Login'
 import PageNotFound from '../src/pages/PageNotFound'
 import Settings from '../src/pages/Settings'
 import Users from '../src/pages/Users'
+import AppLayout from "./ui/AppLayout"
 
 function App() {
   return (
     <>
     <GlobalStyles />
-    
     <BrowserRouter>
       <Routes>
-        <Route index element={<Navigate replace to='dashboard' />}  /> 
-        <Route path='/dashboard' element={<Dashboard />}  /> 
-        <Route path='/bookings' element={<Bookings />}  /> 
-        <Route path='/cabins' element={<Cabins />}  />
-        <Route path='/users' element={<Users />}  />
-        <Route path='/settings' element={<Settings />}  />
-        <Route path='/account' element={<Account />}  />
-        <Route path='/login' element={<Login />}  />
-        <Route path='*' element={<PageNotFound />}  />
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to='dashboard' />}  /> 
+          <Route path='/dashboard' element={<Dashboard />}  /> 
+          <Route path='/bookings' element={<Bookings />}  /> 
+          <Route path='/cabins' element={<Cabins />}  />
+          <Route path='/users' element={<Users />}  />
+          <Route path='/settings' element={<Settings />}  />
+          <Route path='/account' element={<Account />}  />
+        </Route>
+          <Route path='/login' element={<Login />}  />
+          <Route path='*' element={<PageNotFound />}  />
       </Routes>
     </BrowserRouter>
     </>
