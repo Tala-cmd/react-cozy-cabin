edf8ee67271d98972ba2c5f60011a3113ab85296
Tala <70854429+Tala-cmd@users.noreply.github.com>
2024-05-17 20:11:58 +0400

    Displaying toasts

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Settings from '../src/pages/Settings'
 import Users from '../src/pages/Users'
 import AppLayout from "./ui/AppLayout"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { Toaster } from "react-hot-toast"
 
 const queryClient = new QueryClient({
   defaultOptions:{
@@ -27,6 +28,27 @@ function App() {
       <ReactQueryDevtools initialIsOpen={false} />
 
       <GlobalStyles />
+      <Toaster 
+        position="top-center"
+        gutter={12}
+        containerStyle={{margin: '8px'}}
+        toastOptions={{
+          success:{
+            duration: 3000
+          },
+          error:{
+            duration: 5000
+          },
+          style:{
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '16px 24px',
+            backgroundColor: 'var(--color-grey-0)',
+            color:'var(--color-grey-700)'
+          }
+        }}
+      />
+
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from '../../utils/helpers'
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -50,11 +51,13 @@ function CabinRow({ cabin }) {
   const {isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin, // same as (id)=> deleteCabin(id)
     onSuccess: () => {
+      toast.success('Cabin successfully deleted')
+      
       queryClient.invalidateQueries({
         queryKey: ['cabins']
       }) 
     },
-    onError: (err) => alert(err.message)
+    onError: (err) => toast.error(err.message)
   })
 
   return (
